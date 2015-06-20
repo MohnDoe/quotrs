fad8654f6091cd0e011996f83a25fde143d48a16
Mosby7 <kfnpfeffer@gmail.com>
2015-06-20 18:56:00 +0200

    Add AWS S3 creditentials

--- index.php
+++ index.php
@@ -9,17 +9,32 @@
     // require autoload / composer
     require 'model/core.php';
 
-    // test HashID with quote
-    if(isset($_GET['quote_hashid'])){
-        $Quote = new Quote($_GET['quote_hashid']);
-    }
-    if(isset($_GET['quote_id'])){
-        echo "Quote ID";
-        $Quote = new Quote($_GET['quote_id'], false);
-    }
-    ?>
-<pre>
-    <?php
-        var_dump($Quote);
-    ?>
-</pre>
+?>
+
+<html>
+<head><meta charset="UTF-8"></head>
+<body>
+<h1>S3 upload example</h1>
+<?php
+    $s3 = Aws\S3\S3Client::factory(array(
+           'key' => AWS_ACCESS_KEY_ID,
+           'secret' => AWS_SECRET_ACCESS_KEY
+   ));
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+        // FIXME: add more validation, e.g. using ext/fileinfo
+        try {
+            // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
+            $upload = $s3->upload(S3_BUCKET_NAME, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+            ?>
+            <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
+        <?php } catch(Exception $e) {
+            var_dump($e);
+            ?>
+            <p>Upload error :(</p>
+        <?php } } ?>
+<h2>Upload a file</h2>
+<form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+    <input name="userfile" type="file"><input type="submit" value="Upload">
+</form>
+</body>
+</html>
--- model/core.php
+++ model/core.php
@@ -14,20 +14,34 @@ DEFINE('RATIO_POP_MULTIPLI', 0.99519804434435373165003884241728);
 // connexion db
 if($_SERVER["REMOTE_ADDR"] != "127.0.0.1"){
 	// on heroku
+    // database
     $urlClearDB = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	DEFINE('HOSTNAME', $urlClearDB["host"]);
-	DEFINE('DBNAME', substr($urlClearDB["path"], 1));
-	DEFINE('USER_DB', $urlClearDB["user"]);
-	DEFINE('PASS_DB',$urlClearDB["pass"]);
-	DEFINE('ROOT', '/kunden/homepages/45/d399347765/htdocs/quotrs/');
+    DEFINE('HOSTNAME', $urlClearDB["host"]);
+    DEFINE('DBNAME', substr($urlClearDB["path"], 1));
+    DEFINE('USER_DB', $urlClearDB["user"]);
+    DEFINE('PASS_DB',$urlClearDB["pass"]);
+
+    // file + static
+    DEFINE('S3_BUCKET_NAME', getenv('S3_BUCKET_NAME'));
+    DEFINE('AWS_ACCESS_KEY_ID', getenv('AWS_ACCESS_KEY_ID'));
+    DEFINE('AWS_SECRET_ACCESS_KEY', getenv('AWS_SECRET_ACCESS_KEY'));
+    DEFINE('ROOT', '/kunden/homepages/45/d399347765/htdocs/quotrs/');
 }else{
 	// local
 	$root = $_SERVER["DOCUMENT_ROOT"];
 	if($root[strlen($root)-1] == "/"){
 		$root = substr($root, 0, strlen($root)-1);
 	}
-	DEFINE('ROOT', $root);
+
+    DEFINE('S3_BUCKET_NAME', "qtrs");
+    DEFINE('AWS_ACCESS_KEY_ID', "AKIAITYA6XFVP7OIETTA");
+    DEFINE('AWS_SECRET_ACCESS_KEY', "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");
+
+
+    DEFINE('ROOT', $root);
 	DEFINE('PROJECT_FOLDER', "/quotrs");
+
+    // database
 	DEFINE('HOSTNAME', 'localhost');
 	DEFINE('DBNAME', 'db_quotrap');
 	DEFINE('USER_DB', 'root');
