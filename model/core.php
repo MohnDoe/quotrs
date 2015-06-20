5d178fa8925c141bb08d5066d1fb8e914eff055b
Mosby7 <kfnpfeffer@gmail.com>
2015-06-20 19:01:57 +0200

    Fix ROOT in core.php

--- model/core.php
+++ model/core.php
@@ -6,52 +6,41 @@
 */
     require 'vendor/autoload.php';
 
-session_start();
-header('Content-Type: text/html; charset=UTF-8');
+    session_start();
+    header('Content-Type: text/html; charset=UTF-8');
 
-DEFINE('RATIO_POP_MULTIPLI', 0.99519804434435373165003884241728);
+    DEFINE('RATIO_POP_MULTIPLI', 0.99519804434435373165003884241728);
 
-// connexion db
-if($_SERVER["REMOTE_ADDR"] != "127.0.0.1"){
-	// on heroku
-    // database
-    $urlClearDB = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    DEFINE('HOSTNAME', $urlClearDB["host"]);
-    DEFINE('DBNAME', substr($urlClearDB["path"], 1));
-    DEFINE('USER_DB', $urlClearDB["user"]);
-    DEFINE('PASS_DB',$urlClearDB["pass"]);
+    // connexion db
+    if($_SERVER["REMOTE_ADDR"] != "127.0.0.1"){
+        // on heroku
+        // database
+        $urlClearDB = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        DEFINE('HOSTNAME', $urlClearDB["host"]);
+        DEFINE('DBNAME', substr($urlClearDB["path"], 1));
+        DEFINE('USER_DB', $urlClearDB["user"]);
+        DEFINE('PASS_DB',$urlClearDB["pass"]);
 
-    // file + static
-    DEFINE('S3_BUCKET_NAME', getenv('S3_BUCKET_NAME'));
-    DEFINE('AWS_ACCESS_KEY_ID', getenv('AWS_ACCESS_KEY_ID'));
-    DEFINE('AWS_SECRET_ACCESS_KEY', getenv('AWS_SECRET_ACCESS_KEY'));
-    DEFINE('ROOT', '/kunden/homepages/45/d399347765/htdocs/quotrs/');
-}else{
-	// local
-	$root = $_SERVER["DOCUMENT_ROOT"];
-	if($root[strlen($root)-1] == "/"){
-		$root = substr($root, 0, strlen($root)-1);
-	}
+        // file + static
+        DEFINE('S3_BUCKET_NAME', getenv('S3_BUCKET_NAME'));
+        DEFINE('AWS_ACCESS_KEY_ID', getenv('AWS_ACCESS_KEY_ID'));
+        DEFINE('AWS_SECRET_ACCESS_KEY', getenv('AWS_SECRET_ACCESS_KEY'));
+    }else{
+        // local
 
-    DEFINE('S3_BUCKET_NAME', "qtrs");
-    DEFINE('AWS_ACCESS_KEY_ID', "AKIAITYA6XFVP7OIETTA");
-    DEFINE('AWS_SECRET_ACCESS_KEY', "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");
+        // AWS S3
+        DEFINE('S3_BUCKET_NAME', "qtrs");
+        DEFINE('AWS_ACCESS_KEY_ID', "AKIAITYA6XFVP7OIETTA");
+        DEFINE('AWS_SECRET_ACCESS_KEY', "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");
 
-
-    DEFINE('ROOT', $root);
-	DEFINE('PROJECT_FOLDER', "/quotrs");
-
-    // database
-	DEFINE('HOSTNAME', 'localhost');
-	DEFINE('DBNAME', 'db_quotrap');
-	DEFINE('USER_DB', 'root');
-	DEFINE('PASS_DB','');
-}
-
-    DEFINE('WEBROOT', ROOT.PROJECT_FOLDER);
+        // database
+        DEFINE('HOSTNAME', 'localhost');
+        DEFINE('DBNAME', 'db_quotrap');
+        DEFINE('USER_DB', 'root');
+        DEFINE('PASS_DB','');
+    }
+    DEFINE('ROOT', "qtrs.s3-website-eu-west-1.amazonaws.com");
     DEFINE('SALT_HASHIDS', "12309UJQODJ09ZA8ESQDJLQSJDAZEU");
-    //DEFINE('FOLDER_IMGS', STATIC_URL.'/img');
-    ////DEFINE('FOLDER_COVER_ALBUMS', FOLDER_IMGS.'/albums');
 
 
 
