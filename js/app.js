a039a6f66cc6dfdd4968198d62bc212959400849
Mosby7 <kfnpfeffer@gmail.com>
2015-07-26 16:41:56 +0200

    JS clean up

--- js/app.js
+++ js/app.js
@@ -82,81 +82,6 @@ app.controller('formQuoteController', function ($scope, $http) {
             });
     };
 
-    $scope.getArtists = function (val) {
-        if (typeof(val) != undefined) {
-            return $http.get('api/artists/' + val).then(
-                function (res) {
-                    var artists = [];
-                    angular.forEach(res.data.artists, function (artist) {
-                        artists.push(artist)
-                    })
-                    return artists;
-                }
-            )
-        } else {
-            return [];
-        }
-    };
-    $scope.getSongs = function (val) {
-        var request = './api/songs/' + val;
-        if ($scope.quote.song.artist.existingID != -1) {
-            request = './api/artists/' + $scope.quote.song.artist.existingID + "/songs/" + val
-        }
-        return $http.get(request).then(
-            function (res) {
-                var songs = [];
-                $scope.autocompleteArtists = res.data.artists;
-                angular.forEach(res.data.songs, function (song) {
-                    songs.push(song);
-                });
-                return songs;
-            });
-    };
-
-    $scope.onSelectArtistSong = function ($item, $model, $label) {
-        $scope.quote.song.artist = {
-            name: $item.name,
-            existingID: $item.id,
-            artistIsNew: false
-        }
-
-        // ALBUM
-        $scope.quote.song.album.artist = {
-            name: $item.name,
-            existingID: $item.id,
-            artistIsNew: false,
-            artistIsSameAsSong: true
-        }
-    };
-
-    $scope.onSelectSong = function ($item, $model, $label) {
-        //console.log($item);
-        $scope.quote.song.title = $item.title;
-        $scope.quote.song.url_youtube = $item.url_youtube;
-        $scope.quote.song.songIsNew = false;
-        $scope.quote.song.existingID = $item.id;
-
-        $scope.quote.song.artist.name = $item.Artist.name;
-        $scope.quote.song.artist.existingID = $item.Artist.id;
-        $scope.quote.song.artist.artistIsNew = false;
-
-        $scope.quote.song.album.title = $item.Album.title;
-        $scope.quote.song.album.existingID = $item.Album.id;
-        $scope.quote.song.album.url_cover = $item.Album.url_cover;
-        $scope.quote.song.album.albumIsNew = false;
-        // console.log($scope.quote);
-    };
-
-    $scope.$watch(
-        function () {
-            return $scope.quote.song.artist.name
-        },
-        function (newVal) {
-            $scope.quote.song.album.artist.name = newVal;
-        },
-        true
-    );
-
     $scope.$watch(
         function () {
             return $scope.quote.content;
@@ -165,6 +90,7 @@ app.controller('formQuoteController', function ($scope, $http) {
             var access_token = "uWibAg6C7pcdQpvynsw8zk-fqo7crN5-bckfL0rJ1_SOTRM5BOTKygD4Q-e_ppDz";
             var request = "https://api.genius.com/search?q=" + newVal;
             request += "&access_token=" + access_token;
+
             $http.get(request).then(
                 function (res) {
                     data = res.data;
