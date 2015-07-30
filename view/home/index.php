b50789a76a3709a3686b7fcc2387f0f546238f00
Mosby7 <kfnpfeffer@gmail.com>
2015-07-30 20:49:10 +0200

    Add home template folder

---
+++ view/home/index.php
@@ -0,0 +1,60 @@
+<?php 
+
+
+?>
+<div class="container-quote-of-the-day">
+	<span class="title">Citation du jour</span>
+	<div class="container-quote">
+		<div class="the-quote">
+			<span class="line-quote">Lorem ipsum dolor sit.</span>
+	        <div style = "clear: both; display: table;"></div>
+			<span class="author-quote">Médine</span>
+		</div>
+		<div class="background-quote" back-img="https://unsplash.it/1500?random"></div>
+		<div class="gradient-background-quote"></div>
+	</div>
+</div>
+<div class="container-trending-quotes">
+	<span class="small-title">Citations du moment</span>
+	<div style = "clear: both; display: table;"></div>
+	<div class="container-quote featured-quote">
+		<div class="the-quote">
+			<span class="line-quote">Lorem ipsum dolor sit.</span>
+            <div style = "clear: both; display: table;"></div>
+			<span class="author-quote">Médine</span>
+		</div>
+		<div class="background-quote" back-img="https://unsplash.it/1000?random"></div>
+		<div class="gradient-background-quote"></div>
+		<div class="box-border-white"></div>
+	</div>
+	<div class="container-quote">
+		<div class="the-quote">
+			<span class="line-quote">Lorem ipsum dolor sit.</span>
+            <div style = "clear: both; display: table;"></div>
+			<span class="author-quote">Médine</span>
+		</div>
+		<div class="background-quote" back-img="https://unsplash.it/1050?random2"></div>
+		<div class="gradient-background-quote"></div>
+		<div class="box-border-white"></div>
+	</div>
+	<div class="container-quote">
+		<div class="the-quote">
+			<span class="line-quote">Lorem ipsum dolor sit.</span>
+            <div style = "clear: both; display: table;"></div>
+			<span class="author-quote">Médine</span>
+		</div>
+		<div class="background-quote" back-img="https://unsplash.it/1100?random3"></div>
+		<div class="gradient-background-quote"></div>
+		<div class="box-border-white"></div>
+	</div>
+	<div class="container-quote">
+		<div class="the-quote">
+			<span class="line-quote">Lorem ipsum dolor sit.</span>
+            <div style = "clear: both; display: table;"></div>
+			<span class="author-quote">Médine</span>
+		</div>
+		<div class="background-quote" back-img="https://unsplash.it/1200?random4"></div>
+		<div class="gradient-background-quote"></div>
+		<div class="box-border-white"></div>
+	</div>
+</div>
